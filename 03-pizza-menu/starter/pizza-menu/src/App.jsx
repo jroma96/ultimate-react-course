25a0dd30c1f41589e7aacba3d36518ef519c0ec6
jroma96 <31935178+jroma96@users.noreply.github.com>
2026-01-09 11:52:03 -0600

    changes

--- 03-pizza-menu/starter/pizza-menu/src/App.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/App.jsx
@@ -1,27 +1,83 @@
 /*import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";*/
-import focacciaImg from "./assets/pizzas/focaccia.jpg";
 import pizzaData from "./assets/data.js";
 import "./App.css";
 
 function App() {
-  console.log(pizzaData[0]);
+  return (
+    <div className="container">
+      <Header />
+      <Menu pizzaData={pizzaData} />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizza({ img, name, ingredients }) {
+  return (
+    <div className="pizza div">
+      <h3 className="pizza h3">
+        <b style={{ color: "gray" }}>{name}</b>
+      </h3>
+      <img
+        className="pizza img"
+        src={new URL(`${img}`, import.meta.url).href}
+        alt={name}
+      />
+      <p className="pizza p">{ingredients}</p>
+    </div>
+  );
+}
+
+function Header() {
   return (
     <h1>
-      Hello World! <b>My friends</b>
-      <Pizza />
+      <b>Roma's Pizzeria</b>
     </h1>
   );
 }
 
-function Pizza() {
+function Row({ item1, item2 }) {
+  console.log(item1, item2);
   return (
-    <div>
-      <img src={focacciaImg} alt="focaccia" />
-      <div>{pizzaData[0].name}</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: "2rem",
+      }}
+    >
+      <Pizza
+        name={item1.name}
+        img={item1.photoName}
+        ingredients={item1.ingredients}
+      />
+      <Pizza
+        name={item2.name}
+        img={item2.photoName}
+        ingredients={item2.ingredients}
+      />
     </div>
   );
+}
+
+function Menu({ pizzaData }) {
+  const data = pizzaData;
+  console.log(data);
+  return (
+    <div className="menu">
+      <h2 style={{ fontSize: "48px", color: "gray" }}>Our Menu</h2>
+      <Row item1={data[0]} item2={data[1]} />
+      <Row item1={data[2]} item2={data[3]} />
+      <Row item1={data[4]} item2={data[5]} />
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer className="footer">Currently Open on Guatemala</footer>;
 }
 
 export default App;
